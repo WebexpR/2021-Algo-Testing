3da4a602a161b1247d402e23420d66923299eb3a
Benjamin ROUVILLE <brouville@webexpr.fr>
2021-12-13 14:48:39 +0100

    Adding react hook

---
+++ react-hook/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MotMelee from './components/MotMelee';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mots mélés</h1>
+      <MotMelee />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-hook/src/components/MotMelee.tsx
@@ -0,0 +1,12 @@
+import './MotMelee.scss';
+
+function MotMelee() {
+    
+    return (
+        <div className="content">
+            <h1>Bon courage !</h1>
+        </div>
+    );
+}
+
+export default MotMelee;
